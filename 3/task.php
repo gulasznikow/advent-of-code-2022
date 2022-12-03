8343329a42f1c833413f4a0214b9c37960693b6e
Maciej Sajdok <maciej.sajdok@tsh.io>
2022-12-03 07:51:22 +0100

    3 done

---
+++ 3/task.php
@@ -0,0 +1,22 @@
+<?php
+
+$data = file(__DIR__.'/input.txt');
+
+$sum = 0;
+
+foreach ($data as $entry)
+{
+    $trimmed = trim($entry);
+    $firstCompartment = str_split(substr($trimmed, 0, floor(strlen($trimmed)/2)));
+    $secondCompartment = str_split(substr($trimmed, floor(strlen($trimmed)/2)));
+    $common = implode(array_unique(array_intersect($firstCompartment, $secondCompartment)));
+
+    if (ctype_upper($common))
+    {
+        $sum += ord($common) - 64+ 26;
+    } else {
+        $sum += ord($common)- 96;
+    }
+}
+
+print_r($sum.PHP_EOL);
---
+++ 3/task2.php
@@ -0,0 +1,18 @@
+<?php
+
+$data = file(__DIR__.'/input.txt');
+
+$sum = 0;
+
+foreach (array_chunk($data, 3) as $entry)
+{
+    $common = trim(implode(array_unique(array_intersect(str_split($entry[0]), str_split($entry[1]), str_split($entry[2])))));
+    if (ctype_upper($common))
+    {
+        $sum += ord($common) - 64 + 26;
+    } else {
+        $sum += ord($common)- 96;
+    }
+}
+
+print_r($sum.PHP_EOL);

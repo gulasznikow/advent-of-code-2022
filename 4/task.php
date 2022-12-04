d2cbaa15eb7c72338887810929034f4f1a573e32
Maciej Sajdok <maciej.sajdok@tsh.io>
2022-12-04 08:36:09 +0100

    4 done

---
+++ 4/task.php
@@ -0,0 +1,21 @@
+<?php
+function overlap($minBoundary1, $maxBoundary1, $minBoundary2, $maxBoundary2): bool
+{
+    return (($minBoundary1 <= $minBoundary2) && ($maxBoundary1>=$maxBoundary2))
+        || (($minBoundary2 <= $minBoundary1) && ($maxBoundary2>=$maxBoundary1));
+}
+
+$data = file(__DIR__.'/input.txt');
+
+$overlappedPairs = 0;
+
+foreach ($data as $entry)
+{
+    [$first, $second] = explode(',', $entry);
+    [$min1, $max1] = explode('-', $first);
+    [$min2, $max2] = explode('-', $second);
+
+    $overlappedPairs += overlap((int)$min1, (int)$max1, (int)$min2, (int)$max2) ? 1 : 0;
+}
+
+print_r($overlappedPairs);

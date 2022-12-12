a7edd4c1d58e9068941e3be10cba95f14bb595c2
Maciej Sajdok <maciej.sajdok@tsh.io>
2022-12-12 08:23:44 +0100

    12

---
+++ src/12/task.php
@@ -0,0 +1,74 @@
+<?php
+
+require '../../vendor/autoload.php';
+require 'Algo/Dijkstra.php';
+
+$directions = [
+    [0, -1],
+    [-1, 0], [1, 0],
+    [0,1 ],
+];
+
+$data = file(__DIR__ . '/input.txt');
+
+$map = [];
+$start = [0,0];
+$end = [0,0];
+
+foreach ($data as $y => $row) {
+    foreach (str_split(trim($row)) as $x => $column) {
+        if ($column === 'S') {
+            $map[$x][$y] = 'a';
+            $start = [$x, $y];
+            continue;
+        }
+        if ($column === 'E') {
+            $map[$x][$y] = 'z';
+            $end = [$x, $y];
+            continue;
+        }
+        $map[$x][$y] = $column;
+    }
+}
+
+//Add starting point to queue
+$q = new SplQueue();
+$q->enqueue([0,$start]);
+$v[$start[0].'-'.$start[1]] = 1;
+
+$step = 0;
+while($q->count()>0)
+{
+    //Take first queue element and decompose it to weight and coords
+    $e = $q->shift();
+    $i = $e[0];
+    $x = $e[1][0];
+    $y = $e[1][1];
+    //We iterate trough every direction
+    foreach ($directions as $direction)
+    {
+        $newX = $x+$direction[0];
+        $newY = $y+$direction[1];
+
+        //Check if visited coords are out of bound
+        if ($newX < 0 || $newY < 0 || $newX>=count($map) || $newY>=count($map[0])) continue;
+
+        //Check if we visited it already
+        if (isset($v[$newX.'-'.$newY])){
+          continue;
+        }
+
+        //Check if its even traversable to
+        if ((ord($map[$newX][$newY]) - ord($map[$x][$y])) > 1 ) continue;
+
+        //Check if the visited coords are the destination coords and end it
+        if ($newX === $end[0] && $newY === $end[1])
+        {
+            print_r($i+1);
+            die();
+        }
+        //Add visited coords to visited array and push new entry to queue with distance +1 and new coords
+        $v[$newX.'-'.$newY]= 1;
+        $q->push([$i+1,[$newX,$newY]]);
+    }
+}
